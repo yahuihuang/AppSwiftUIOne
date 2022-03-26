cfcb7fbe53c0c42dcb998b6e25fd2561cdb12488
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 00:14:56 +0800

    傳入參數的測試

--- AppSwiftUIOne/ContentView.swift
+++ AppSwiftUIOne/ContentView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State var activeTab: Int = 0
+    @State var displayString: String = "這是傳入參數的測試Ｉ"
     var body: some View {
         TabView(selection: $activeTab) {
             Text("in Page \(activeTab)").tabItem { Image(systemName: "list.bullet")}.tag(1)
             
             Text("in Page \(activeTab)").tabItem {
                 Image(systemName: "list.bullet")}.tag(2)
+            
+            Text(displayString).tabItem {
+                Image(systemName: "list.bullet")}.tag(3)
         }
     }
 }
