13bcf720a54207e459be11924e6dd2cdc471762b
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-26 23:11:13 +0800

    Initial Commit

---
+++ AppSwiftUIOne/AppSwiftUIOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIOneApp.swift
+//  AppSwiftUIOne
+//
+//  Created by grace on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
