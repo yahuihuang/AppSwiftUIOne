b936f76dde74ad35857beffea6e84262a741ed3f
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 11:32:57 +0800

    設定調色盤畫面

--- AppSwiftUIOne/AppSwiftUIOneApp.swift
+++ AppSwiftUIOne/AppSwiftUIOneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppSwiftUIOneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(displayString: "傳入參數測試-production!")
+            ContentView()
         }
     }
 }
--- AppSwiftUIOne/ContentView.swift
+++ AppSwiftUIOne/ContentView.swift
@@ -9,37 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
 //    @State var activeTab: Int = 0
-    @State var displayString: String = ""
+//    @State var displayString: String = ""
+    @State var redBar: Float = 0
+    @State var greenBar: Float = 0
+    @State var blueBar: Float = 0
     var body: some View {
-        
         VStack {
-            // 用Spacer()分三等分
-            Spacer()
-            Text(displayString)
-                .foregroundColor(Color.gray)
-            Spacer()
             HStack {
-                Text("請輸入字串: ")
-                    .padding(.leading, 10.0)
-                TextField("", text: $displayString)
+                Text("R: \(Int(redBar * 255))").font(.largeTitle)
+                Text("G: \(Int(greenBar * 255))").font(.largeTitle)
+                Text("B: \(Int(blueBar * 255))").font(.largeTitle)
             }
             Spacer()
+            VStack {
+                Slider(value: $redBar)
+                Slider(value: $greenBar)
+                Slider(value: $blueBar)
+            }.padding(.all, 30)
         }
-        
-//        TabView(selection: $activeTab) {
-//            Text("in Page \(activeTab)").tabItem { Image(systemName: "list.bullet")}.tag(1)
-//
-//            Text("in Page \(activeTab)").tabItem {
-//                Image(systemName: "list.bullet")}.tag(2)
-//
-//            Text(displayString).tabItem {
-//                Image(systemName: "list.bullet")}.tag(3)
-//        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(displayString: "傳入參數測試-preview!")
+        ContentView()
     }
 }
