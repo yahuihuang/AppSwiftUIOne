52ab183499b809de83e62473a1ef6e1d506f8319
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 13:05:33 +0800

    精簡寫法

--- AppSwiftUIOne/ContentView.swift
+++ AppSwiftUIOne/ContentView.swift
@@ -16,24 +16,50 @@ struct ContentView: View {
         ZStack {
             // 背景色
             Color(red: redBar, green: greenBar, blue: blueBar)
-            
-            VStack {
-                HStack {
-                    Text("R: \(Int(redBar * 255))").font(.largeTitle)
-                    Text("G: \(Int(greenBar * 255))").font(.largeTitle)
-                    Text("B: \(Int(blueBar * 255))").font(.largeTitle)
-                }
-                Spacer()
-                VStack {
-                    Slider(value: $redBar)
-                    Slider(value: $greenBar)
-                    Slider(value: $blueBar)
-                }.padding(.all, 30)
-            }
+//            ColorContainer()
+            ctrl
         }
-        
     }
+    
+    var ctrl: some View {
+        VStack {
+            HStack {
+                Text("R: \(Int(redBar * 255))").font(.largeTitle)
+                Text("G: \(Int(greenBar * 255))").font(.largeTitle)
+                Text("B: \(Int(blueBar * 255))").font(.largeTitle)
+            }
+            Spacer()
+            VStack {
+                Slider(value: $redBar)
+                Slider(value: $greenBar)
+                Slider(value: $blueBar)
+            }.padding(.all, 30)
+        }
+    }
+    
 }
+
+//struct ColorContainer: View {
+//    @State var redBar: Double = 0.5
+//    @State var greenBar: Double = 0.5
+//    @State var blueBar: Double = 0.5
+//
+//    var body: some View {
+//        VStack {
+//            HStack {
+//                Text("R: \(Int(redBar * 255))").font(.largeTitle)
+//                Text("G: \(Int(greenBar * 255))").font(.largeTitle)
+//                Text("B: \(Int(blueBar * 255))").font(.largeTitle)
+//            }
+//            Spacer()
+//            VStack {
+//                Slider(value: $redBar)
+//                Slider(value: $greenBar)
+//                Slider(value: $blueBar)
+//            }.padding(.all, 30)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
